6af229820524954f12b89bb8c88ffb1b1f32618e
haile eshetu <michaelhaile1222@gmail.com>
2025-02-13 09:24:31 +0300

    adding the button

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,14 +1,14 @@
-const API_KEY=process.env.REACT_APP_API_KEY;
+
 const requests={
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-    fetchPopularTvShows: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+    fetchTrending: `/trending/all/week?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&with_genres=99`,
+    fetchPopularTvShows: `/tv/popular?api_key=ccf99bb7baa7727b992fb24d66bd2aeb&language=en-US&page=1`
     
 
 
